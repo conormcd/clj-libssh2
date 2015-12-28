28ef1fe43b527f3d3d1c94fe3993c89ea8cd3b92
Conor McDermottroe <conor@mcdermottroe.com>
2015-12-28 20:26:36 +0000

    Add license details to project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clj-libssh2 "0.1.0-SNAPSHOT"
   :description "A Clojure wrapper around libssh2"
   :url "https://github.com/conormcd/clj-libssh2"
+  :license {:name "BSD"
+            :url "https://github.com/conormcd/clj-libssh2/blob/master/LICENSE"}
   :pedantic? :abort
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [net.n01se/clojure-jna "1.0.0"]])
