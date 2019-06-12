a9823e9d272f3fc7033cdde0620cf786783a67c8
Conor McDermottroe <conor@circleci.com>
2019-06-12 07:58:52 +0100

    Upgrade lein-codox to 0.10.7
    This is needed in order to work with leiningen 2.9.x

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  [digest "1.4.4"]
                  [net.n01se/clojure-jna "1.0.0"]
                  [robert/hooke "1.3.0"]]
-  :profiles {:dev {:plugins [[lein-codox "0.9.1"]]}
+  :profiles {:dev {:plugins [[lein-codox "0.10.7"]]}
              :test {:dependencies [[clojure-test-junit-output "0.1.3"]]
                     :jvm-opts ["-Djava.util.logging.config.file=test/logging.properties"]}}
   :deploy-repositories ^:replace [["clojars" {:url "https://clojars.org/repo"
